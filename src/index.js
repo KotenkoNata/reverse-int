6bd7604375d3c456a551b8fd4066b99a253ff514
KotenkoNata <kotenkonata8@gmail.com>
2021-12-29 12:05:03 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let convertN = n.toString();
+
+  let result = "";
+
+   for( let i = convertN.length - 1; i >= 0; i--){
+       result = result + convertN[i];
+   }
+
+  return parseInt(result);
 }
